1d17caf98ffa22880c9fd5f48957f7837e95cc96
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-10-13 22:52:04 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,11 +1,3 @@
-a=[]
-n= int(input("Enter the number of elements: "))
-for x in range(0,n):
-    element=int(input("Enter element" + str(x+1) + ": "))
-    a.append(element)
-b=[sum(a[0:x+1]) for x in range(0,len(a))]
-print("The Initial list is: ",a)
-print("The final list is: ",b)n=int(input("Enter a number to be checked:- "))
 temp=n
 rev=0
 while(n>0):
