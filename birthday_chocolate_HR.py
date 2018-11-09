b03e5aaa85e11511d3ddf30d14778ea71664bfb8
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-09 22:58:04 +0530

    Add files via upload

---
+++ birthday_chocolate_HR.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+    count=0
+    for i in range(0,len(s)):
+        sum=0
+        for j in range(0,m):
+            a=i+j
+            if a<len(s):
+                sum=sum+s[a]
+        if sum==d:
+            count=count+1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    dm = input().rstrip().split()
+
+    d = int(dm[0])
+
+    m = int(dm[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
