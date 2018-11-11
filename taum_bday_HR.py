0e0607969432445636e2542e113c02fd998e2292
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-11 22:22:17 +0530

    Add files via upload

---
+++ taum_bday_HR.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the taumBday function below.
+def taumBday(b, w, bc, wc, z):
+    cost=0
+    if bc>(wc+z):
+        bc=wc+z
+    if wc>(bc+z):
+        wc=bc+z
+    cost=(b*bc)+(w*wc)
+    return cost
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        bw = input().split()
+
+        b = int(bw[0])
+
+        w = int(bw[1])
+
+        bcWcz = input().split()
+
+        bc = int(bcWcz[0])
+
+        wc = int(bcWcz[1])
+
+        z = int(bcWcz[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
