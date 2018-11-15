2edff53a08c145bc90d0d30cc7a6332a2db5d49b
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-15 23:34:37 +0530

    Add files via upload

---
+++ viral_advertising_HR.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    total=0
+    a=5
+    for i in range(1,n+1):
+        like=a//2
+        total=total+like
+        a=like*3
+    return total
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
