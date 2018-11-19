7344e94d46808e0e46b754cc60597d1b5cb4ea2f
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-19 23:37:13 +0530

    Add files via upload

---
+++ electronics_shop_HR.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the getMoneySpent function below.
+#
+def getMoneySpent(keyboards, drives, b):
+    #
+    # Write your code here.
+    #
+    sum=0
+    ma=0
+    for i in range(0,len(keyboards)):
+        for j in range(0,len(drives)):
+            sum=keyboards[i]+drives[j]
+            if sum>ma and sum<=b:
+                ma=sum
+    if ma==0:
+        return -1
+    else:
+        return ma
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    bnm = input().split()
+
+    b = int(bnm[0])
+
+    n = int(bnm[1])
+
+    m = int(bnm[2])
+
+    keyboards = list(map(int, input().rstrip().split()))
+
+    drives = list(map(int, input().rstrip().split()))
+
+    #
+    # The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+    #
+
+    moneySpent = getMoneySpent(keyboards, drives, b)
+
+    fptr.write(str(moneySpent) + '\n')
+
+    fptr.close()
