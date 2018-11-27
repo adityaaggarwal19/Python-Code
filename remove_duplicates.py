6b01a2e390003d48f457178face167ed66464f93
adityaaggarwal19 <33057627+adityaaggarwal19@users.noreply.github.com>
2018-11-27 21:00:17 +0530

    Add files via upload

---
+++ remove_duplicates.py
@@ -0,0 +1,8 @@
+'''
+Objective:- Remove Duplicates from the list
+'''
+#Approach:- Use the set function as the set contains the unique elements
+def rem(lis):
+    print(list(set(lis)))
+rem([1,2,3,1,2,3,4,5,6,7,8,9,5,6,7,5,1])
+    
